8ff1890548a33ebb125f243a966428f8956963b1
santisans123 <santitok21@gmail.com>
2023-07-15 17:19:06 +0700

    project and to do list

--- lib/screen/bottom_nav_screen.dart
+++ lib/screen/bottom_nav_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:spo_balaesang/screen/application_screen.dart';
 import 'package:spo_balaesang/screen/home_page.dart';
 import 'package:spo_balaesang/screen/home_screen.dart';
+import 'package:spo_balaesang/screen/project/project_screen.dart';
+import 'package:spo_balaesang/screen/todo_list/personal/todo_list_personal.dart';
 
 class BottomNavScreen extends StatefulWidget {
   @override
@@ -9,45 +11,47 @@ class BottomNavScreen extends StatefulWidget {
 }
 
 class _BottomNavScreenState extends State<BottomNavScreen> {
-  final List<Widget> _screens = [HomePage(), ApplicationScreen()];
+  final List<Widget> _screens = [HomePage(), TodoListPersonal(), ProjectScreen(), ApplicationScreen()];
   int _currentIndex = 0;
 
   final Map<String, dynamic> _bottomNavItems = {
     'Beranda': Icons.home_filled,
+    'ToDoList': Icons.list,
+    'Project': Icons.folder,
     'Aplikasi': Icons.apps_rounded,
   };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: IndexedStack(
-          index: _currentIndex,
-          children: _screens,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.blueAccent,
+        selectedLabelStyle: const TextStyle(
+          color: Colors.blueAccent, fontWeight: FontWeight.bold,),
+        unselectedLabelStyle: const TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.normal,
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) => setState(() => _currentIndex = index),
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          showUnselectedLabels: true,
-          selectedItemColor: Colors.blueAccent,
-          selectedLabelStyle: const TextStyle(
-              color: Colors.blueAccent, fontWeight: FontWeight.bold,),
-          unselectedLabelStyle: const TextStyle(
-            color: Colors.grey,
-            fontWeight: FontWeight.normal,
+        unselectedItemColor: Colors.grey,
+        items: _bottomNavItems
+            .map((key, value) => MapEntry(
+          key,
+          BottomNavigationBarItem(
+            label: key,
+            icon: Icon(value as IconData),
           ),
-          unselectedItemColor: Colors.grey,
-          items: _bottomNavItems
-              .map((key, value) => MapEntry(
-                    key,
-                    BottomNavigationBarItem(
-                      label: key,
-                      icon: Icon(value as IconData),
-                    ),
-                  ),)
-              .values
-              .toList(),
-        ),);
+        ),)
+            .values
+            .toList(),
+      ),);
   }
 }
